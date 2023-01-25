97d18c33682797ebb0c387df7b572c22fd30dce8
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2023-01-25 16:55:23 +0100

    ajout de l'ex 4 pas commancer

---
+++ Exercice4.php
@@ -0,0 +1,22 @@
+<?php
+// Schneider Arthur
+declare(strict_types = 1);
+
+// Question 19
+function creeTableau()
+
+// Question 20
+
+// Question 21
+
+// Question 22
+
+// Question 23
+
+// Question 24
+
+// Question 25
+
+// Question 26
+
+// Question 27
